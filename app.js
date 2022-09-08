b2ef4be27332f91e3259c42052453a7435855089
claud7e <cbiver@gmail.com>
2022-09-08 14:36:00 +0200

    Include submit functionality

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const ejs = require("ejs");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://localhost:27017/urlDB");
 
@@ -19,7 +22,9 @@ const model = mongoose.model("urls", schema);
 // This is the rerouting part
 // it takes the short handle, looks it up in the database, and returns the URL
 app.route("/:url").get((req, res) => {
+	// make a request to mongodb
 	model.findOne({ short: req.params.url }, (err, docs) => {
+		// filter database errors
 		if (err) {
 			res.send(err);
 			return;
@@ -29,15 +34,15 @@ app.route("/:url").get((req, res) => {
 				res.send("No such entry");
 				return;
 			} else {
-				// Update the counter first
+				// Update the counter, then redirect
 				model.updateOne(
 					{ short: req.params.url },
-					{ views: (docs.views += 1) },
+					{ visits: (docs.visits += 1) },
 					(updateError, updateResult) => {
 						if (updateError) {
 							res.send(updateError);
 						} else {
-                            // if all went well, redirect user
+							// if all went well, redirect user
 							res.redirect(docs.url);
 						}
 					}
@@ -47,10 +52,40 @@ app.route("/:url").get((req, res) => {
 	});
 });
 
-app.route("/").get((req, res) => {
-	// TODO a Form to create a new reroute
-	res.send("Hello, world!");
-});
+// generating random id
+// It takes one parameter: number
+// how many letters will the word have?
+
+function genId(number) {
+	let result = "";
+	let chars =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for (let i = 0; i < number; i++) {
+		result += chars.charAt(Math.floor(Math.random() * chars.length));
+	}
+	return result;
+}
+
+app.route("/")
+	.get((req, res) => {
+		// TODO a Form to create a new reroute
+		res.render("form");
+	})
+	.post((req, res) => {
+		// TODO save the new reroute in database
+		const url = req.body.url;
+		const short = genId(5);
+
+		const newUrl = new model({
+			url: url,
+			short: short,
+			visits: 0,
+		});
+		newUrl.save().then(() => {
+			console.log("New entry saved");
+		});
+		res.render("submitted", { url: short });
+	});
 
 app.listen(3000, () => {
 	console.log("Server up and running");
